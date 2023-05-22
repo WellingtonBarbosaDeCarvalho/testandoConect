a85fb9e1f8254b318f3a4c2299bc17ecee972db9
WellingtonBarbosaDeCarvalho <barbosadecarvalhow@gmail.com>
2023-05-22 15:45:21 -0300

    privacity

--- src/pages/PrivacityForms/Privacity.jsx
+++ src/pages/PrivacityForms/Privacity.jsx
@@ -7,14 +7,53 @@ export default function Privacity() {
         <aside className="containerMenuHeader">
           <Menu />
         </aside>
-        <h1>Política de privacidade</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nisi sem, consequat ut est a, mattis maximus est. Mauris lacinia, arcu sit amet mattis maximus, leo lacus luctus lectus, vel dapibus dolor nisl non mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean eget ex ut lorem sodales semper a id risus. In hac habitasse platea dictumst. Suspendisse auctor sapien nec arcu auctor eleifend quis sit amet leo. Pellentesque tincidunt eget diam at ullamcorper. Maecenas quis vestibulum orci, a cursus est.
-            Ut at est vehicula, interdum arcu in, scelerisque enim. Mauris hendrerit dui odio, id mollis mauris accumsan id. Duis tempus congue nunc, quis pulvinar libero viverra in. Aliquam nec est non neque luctus iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque tempus mattis quam eu pharetra. Fusce bibendum eleifend libero. Donec ut orci eu ipsum malesuada dapibus. Nulla interdum, tortor et tempus suscipit, sem ex gravida metus, sed tristique massa lacus gravida sem. Ut et dictum quam. Nunc mattis, quam ut blandit sagittis, metus magna cursus quam, vel dignissim massa est et ex. In nisi tortor, pellentesque a mauris sed, pharetra pulvinar turpis. Nunc eu elit pellentesque, dignissim felis quis, gravida velit.
-            Curabitur ac lacus dapibus, convallis tortor vitae, dictum tellus. Curabitur hendrerit velit massa. Nulla facilisi. Nulla aliquam molestie faucibus. Donec vulputate arcu non ipsum tincidunt, eu imperdiet tellus fermentum. Donec in magna ut mi eleifend mollis eu non eros. Proin viverra id massa et mollis. Sed aliquam vulputate blandit. Sed auctor purus sit amet magna malesuada maximus a in purus. Nunc scelerisque mauris sed erat aliquam, et lacinia est volutpat. Duis pharetra suscipit dui non porta. Nam porttitor, est facilisis pretium vestibulum, arcu ex aliquet odio, et mollis quam urna a eros.
-            Sed sollicitudin ante ac molestie mattis. Praesent nec imperdiet justo. Morbi leo sem, pretium eu finibus ac, gravida ut sem. Donec blandit dictum lobortis. Donec pellentesque lobortis rutrum. Nulla pretium rutrum enim, vitae aliquam arcu varius in. Etiam felis mauris, semper et nisl id, condimentum iaculis orci.
-            Pellentesque dapibus leo orci, ut ullamcorper sapien pellentesque vitae. Morbi et auctor diam. Nulla ligula ipsum, feugiat ut iaculis nec, consectetur eget massa. Quisque condimentum metus tristique, pharetra augue vitae, efficitur orci. Aenean vulputate velit tellus, eget convallis enim egestas et. Vivamus eu diam nibh. In mattis imperdiet lacus, sit amet laoreet ligula tristique quis. Fusce eros lacus, luctus a vehicula vitae, tincidunt eget massa.
-        </p>
+        <div>
+          <h1>Política de privacidade</h1>
+        </div>
+        <div className="containerPrivacityParagraph">
+          <p>
+          Para o Conectando Soluções, CNPJ 50.535.094/0001-71, situado em Rua Guapeva, número 104, Água Rasa,( São Paulo-SP) a sua privacidade é de grande importância. Por isso a política do Conectando Soluções, preza respeitar a privacidade em relação
+          a qualquer informação sua que possamos coletar no nosso site.
+          Solicitamos informações pessoais apenas quando, realmente é necessário
+          para lhe fornecer um serviço. Realizamos por meios justos e legais,
+          com o seu conhecimento e consentimento.
+          Também informamos por que estamos coletando e como será usado.
+          <br/>
+          <br/>
+          Apenas retemos as informações coletadas pelo tempo necessário para fornecermos o serviço
+          solicitado, logo após sua ultilização os dados serão deletados completamente. Quando armazenamos
+          dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos,
+          bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
+          Não compartilhamos informações de identificação pessoal publicamente ou com terceiros,
+          a não ser quando exigido por lei.
+          <br/>
+          <br/>
+          Ao contar com nossos serviços, você entende que colheremos e usaremos suas informações
+          pessoais nas formas descritas nesta Política, em conformidade com a lei Geral de Proteção
+          de Dados Pessoais ( Lei 13.709/18), o Marco Civil da Internet (Lei 12.965/14),
+          e as demais normas do ordenamento jurídico brasileiro aplicáveis.
+          <br/>
+          <br/>
+          Você é livre para recusar a nossa solicitação de informações pessoais,
+          entendendo que talvez não possamos fornecer alguns dos serviços desejados.
+          <br/>
+          <br/>
+          O uso continuado de nosso site será considerado, como aceitação de nossas práticas em torno
+          de privacidade e informações pessoais. Se você tiver alguma dúvida, sobre como lidamos com
+          dados do usuário e informações pessoais, entre em contato com uns dos nossos meios de
+          comunicação. 
+          <br/>
+          <br/>
+          Sobre o pagamento dos nossos serviços, contamos com uma plataforma de pagamento
+          parceiras (Mercado Pago). Ciente disso, você autoriza e concorda que seus dados cadastrais,
+          serão utilizados para a realização do pagamento.
+          Reembolso é somente em até sete dias após o pagamento.
+          <br/>
+          <br/>
+          Informamos desde já, que podem haver alterações no conteúdo dessa política ou em parte dela,
+          para seu ajuste e acréscimo. Caso não concorde com o que foi modificado, é possivel realizar o cancelamento dos nossos serviços, dentro do prazo estabelecido
+          </p>
+        </div>
       </section>
   )
 }
